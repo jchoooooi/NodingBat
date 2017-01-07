2ec39f326805a6ba4d30e27dafcaaacf7921a6f1
jchoi <jchoi.yoon@gmail.com>
2017-01-07 17:22:21 -0500

    changed spacing

--- in3050.js
+++ in3050.js
@@ -1,14 +1,12 @@
-
 // Given 2 int values, return true if they are both in the range 30..40 inclusive,
 // or they are both in the range 40..50 inclusive.
 
-var in3050=function(a ,b) { 
-  if (a<=40 && a>=30 && b<=40 && b>=30){
+var in3050 = function(a, b) { 
+  if (a <= 40 && a >= 30 && b <= 40 && b >= 30){
     return true;
-  } else if (a<=50 && a>=40 && b<=50 && b>=40){
+  } else if (a <= 50 && a >= 40 && b <= 50 && b >= 40){
   	return true;
   } else {
     return false;
   }
-  
 }
