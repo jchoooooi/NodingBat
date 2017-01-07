d5dfcee8dde40f93447aed74fd12307d46ee475e
jchoi <jchoi.yoon@gmail.com>
2017-01-07 17:31:54 -0500

    fixed formatting

--- sumDouble.js
+++ sumDouble.js
@@ -1,11 +1,11 @@
 // Given two int values, return their sum. Unless the two values are the same,
 // then return double their sum.
 
-var sumDouble=function(a ,b) { 
-  if (a == b){
-    return (a+b)*2;
+var sumDouble = function(a, b) { 
+  if (a == b) {
+    return (a + b) * 2;
   } else {
-    return a+b;
+    return a + b;
   }
   
 }
